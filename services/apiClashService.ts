c601d0d40d6b82cf2e31ea558df7a5ae169658b2
Hernando Pina <nandodev98@gmail.com>
2025-04-23 01:50:48 +0200

    Update base URL to use HTTPS for improved security

--- services/apiClashService.ts
+++ services/apiClashService.ts
@@ -1,6 +1,6 @@
 // services/apiClashService.ts
 
-const baseUrl = 'http://34.57.116.166:3000';
+const baseUrl = 'https://34.57.116.166:3000';
 
 export const APIClashService = {
   getClanInfo: async (clanTag: string) => {
