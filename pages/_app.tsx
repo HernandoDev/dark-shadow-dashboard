460e61bb9bc67bd847aca03e16ffbf8a5bf92d04
Hernando Pina <nandodev98@gmail.com>
2025-04-27 08:10:53 +0200

    feat: add new logo images for branding enhancement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css';
 import '../styles/login.css'; // Mueve la importación aquí
-import type {AppProps} from 'next/app';
-import {createTheme, NextUIProvider} from '@nextui-org/react';
-import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import {Layout} from '../components/layout/layout';
+import type { AppProps } from 'next/app';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { Layout } from '../components/layout/layout';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
@@ -21,7 +21,7 @@ const darkTheme = createTheme({
    },
 });
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
    const router = useRouter();
 
    useEffect(() => {
@@ -41,6 +41,22 @@ function MyApp({Component, pageProps}: AppProps) {
          }}
       >
          <NextUIProvider>
+            <div
+               style={{
+                  backgroundImage: "url('/logo-fondo-removebg-preview.png')",
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  opacity: 0.2, // Incrementa la opacidad para mayor visibilidad
+
+                  zIndex: 0, // Asegúrate de que el fondo esté detrás del contenido
+               }}
+            />
             <Layout>
                <Component {...pageProps} />
             </Layout>
