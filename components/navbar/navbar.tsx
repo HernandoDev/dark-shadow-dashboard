332443ffb5cdf2762cdf23091126cd6d52ba575b
Hernando Pina <nandodev98@gmail.com>
2025-04-30 12:30:17 +0200

    feat: enhance Navbar button with new styles and hover effects for improved user interaction

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -114,27 +114,33 @@ export const NavbarWrapper = ({ children }: Props) => {
                </div>
             </Navbar.Content>
           
-            <Navbar.Content>
+            <Navbar.Content
+               css={{
+                  justifyContent: 'flex-end', // Align the button to the right
+                  '@md': {
+                     justifyContent: 'center', // Center the button on larger screens
+                  },
+               }}
+            >
                {isAuthenticated && (
                   <button
                      onClick={() => {
                         localStorage.clear(); // Clear all localStorage items
                         window.location.href = '/login';
                      }}
+                     className="Btn"
                      style={{
-                        padding: '10px 20px',
-                        backgroundColor: '#dc2626',
-                        color: 'white',
-                        border: 'none',
-                        borderRadius: '8px',
-                        cursor: 'pointer',
-                        fontWeight: 'bold',
-                        transition: 'background-color 0.3s ease',
+                        ...(typeof window !== 'undefined' && window.innerWidth > 768
+                           ? { left: '-10dvw' } // Apply only for web version
+                           : {}),
                      }}
-                     onMouseOver={(e) => (e.currentTarget.style.backgroundColor = '#b91c1c')}
-                     onMouseOut={(e) => (e.currentTarget.style.backgroundColor = '#dc2626')}
                   >
-                     Cerrar Sesión
+                     <div className="sign">
+                        <svg viewBox="0 0 512 512">
+                           <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+                        </svg>
+                     </div>
+                     <div className="text">Cerrar Sesión</div>
                   </button>
                )}
             </Navbar.Content>
