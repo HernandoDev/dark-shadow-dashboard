9536580810930736781778aac87562c6d0e8a2bc
Hernando Pina <nandodev98@gmail.com>
2025-08-26 14:07:22 +0200

    fix: adjust disadvantage calculation logic in player performance metrics

--- components/home/content.tsx
+++ components/home/content.tsx
@@ -249,7 +249,7 @@ export const Content = () => {
                      playerMap[member.tag].desventaja += 1 + memberUpdate.townhallLevel - opponent.townhallLevel;
                   }
                   if (TH_INFERIOR && attack.stars === 3) {
-                     playerMap[member.tag].desventaja += (memberUpdate.townhallLevel - opponent.townhallLevel) / 1.5;
+                     playerMap[member.tag].desventaja += (memberUpdate.townhallLevel - opponent.townhallLevel) / 1.4;
                   }
                   playerMap[member.tag].totalStars += attack.stars;
                   playerMap[member.tag].totalDestruction += attack.destructionPercentage;
